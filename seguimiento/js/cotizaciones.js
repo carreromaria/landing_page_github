367ddd91c20e607999b6b43868b90b1c7317ad71
Maria Carrero <98425386+carreromaria@users.noreply.github.com>
2026-09-22 13:29:08 -0300

    Update import version for documentos-cotizacion.js

--- seguimiento/js/cotizaciones.js
+++ seguimiento/js/cotizaciones.js
@@ -17,7 +17,7 @@ import {
 import { mejorarSelect } from './components/dropdown-linence.js';
 // El diseño de los documentos COT y DC vive en un solo archivo compartido
 // con el módulo Documentación (js/documentos-cotizacion.js).
-import { htmlCotizacion, htmlDescripcion, imprimirDocumentoPdf } from './documentos-cotizacion.js?v=2';
+import { htmlCotizacion, htmlDescripcion, imprimirDocumentoPdf } from './documentos-cotizacion.js?v=4';
 
 // ---------- Estado ----------
 
